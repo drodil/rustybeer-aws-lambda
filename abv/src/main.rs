b6b5d5dc14975d77effa26975fc78b0a412285d8
Heikki Hellgren <heikki.hellgren@op.fi>
2020-10-04 11:11:33 +0300

    Fix request handling
    Seems like AWS lambda packs it inside "body" of the request (just like
    response). There is also other fields that the AWS sends but let's dig
    them later.

--- abv/src/main.rs
+++ abv/src/main.rs
@@ -3,17 +3,12 @@ use rustybeer::calculators::abv;
 use std::collections::HashMap;
 use lambda::{handler_fn, Context};
 use serde::{Serialize, Deserialize};
+use serde::de::{DeserializeOwned, Deserializer};
+use serde::de::Error as SerdeError;
 
 type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
 
-#[derive(Deserialize, Debug)]
-struct Input {
-    #[serde(rename = "originalGravity")]
-    original_gravity: f32,
-    #[serde(rename = "finalGravity")]
-    final_gravity: f32,
-}
-
+// Move these to separate package
 #[derive(Serialize, Debug)]
 struct LambdaResponse {
     #[serde(rename = "isBase64Encoded")]
@@ -22,6 +17,26 @@ struct LambdaResponse {
     status_code: u16,
     headers: HashMap<String, String>,
     body: String,
+}
+
+pub fn deserialize<'a, T: DeserializeOwned, D: Deserializer<'a>>(
+    deserializer: D,
+) -> Result<T, D::Error> {
+    serde_json::from_str(&String::deserialize(deserializer)?).map_err(SerdeError::custom)
+}
+
+#[derive(Deserialize, Debug)]
+pub struct LambdaRequest<Data: DeserializeOwned> {
+    #[serde(deserialize_with = "deserialize")]
+    body: Data,
+}
+
+#[derive(Deserialize, Debug)]
+struct Input {
+    #[serde(rename = "originalGravity")]
+    original_gravity: f32,
+    #[serde(rename = "finalGravity")]
+    final_gravity: f32,
 }
 
 #[derive(Serialize, Debug)]
@@ -36,10 +51,11 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn calculate_abv(event: Input, _c: Context) -> Result<LambdaResponse, Error> {
+async fn calculate_abv(event: LambdaRequest<Input>, _c: Context) -> Result<LambdaResponse, Error> {
+    let payload = event.body;
     let abv_calc = abv::Abv{};
     let data = Output{
-        abv: abv_calc.calculate_abv(event.original_gravity, event.final_gravity),
+        abv: abv_calc.calculate_abv(payload.original_gravity, payload.final_gravity),
     };
     let response = LambdaResponse{
         is_base64_encoded: false,
